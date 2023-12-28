82a36191a1ba5d965deaaf475b22e3048b46133c
MichelazzoLeonardo <8293569@itisrossi.vi.it>
2023-12-28 12:35:58 +0100

    reset

---
+++ pages/decrypt/vigenere-form.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Encrypt - Vigenere</title>
+    <link rel="stylesheet" type="text/css" href="../../style/style.css">
+</head>
+<body>
+<form action="vigenere-calc.php" method="post">
+    <label class="label">
+        Messaggio criptato:
+        <input type="text" class="input message" name="msg" placeholder="Write here" required autofocus>
+    </label><br>
+    <input type="submit" class="button" value="GO">
+</form>
+<form action="../../index.php" method="post">
+    <input type="submit" class="button" value="<<<">
+</form>
+</body>
+</html>
